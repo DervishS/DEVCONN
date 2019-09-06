e6788c4c37efed6f220242a649602d4339ae5597
Dervish Skenderi <dsk_95@hotmail.com>
2019-09-07 00:06:43 +0200

    Added a way to get github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const request = require('request');
+const config = require('config');
 
 // @route  GET api/profile/me
 // @desc   get current users profile
@@ -331,6 +333,36 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     await profile.save();
 
     res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route  GET  api/profile/github/:username
+// @desc   GET user repos from Github
+// @acces  Public
+router.get('/github/:username', async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body)); // this body will be a regular string, so we suround it with json parse
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
