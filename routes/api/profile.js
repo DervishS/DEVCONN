93e8cfe97a7f5abaa3e26fbca63d082474e55672
Dervish Skenderi <dsk_95@hotmail.com>
2019-09-06 21:39:19 +0200

    Added delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -228,4 +228,24 @@ router.put(
   }
 );
 
+// @route  DELETE  api/profile/experience/:exp_id //: because its a placehorder
+// @desc   DELETE experience from profile
+// @acces  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // get remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    
+  }
+})
+
 module.exports = router;
