702838cd0a147ee7475ca9f1b5fbf27b8e156bcf
Dervish Skenderi <dsk_95@hotmail.com>
2019-09-07 21:04:14 +0200

    Added a way to get and delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -44,4 +44,72 @@ router.post(
   }
 );
 
+// @route  GET api/posts
+// @desc   Get all post
+// @acces  Private if not logged in
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }); //-1 which finds the most recent first
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route  GET api/posts/:ID
+// @desc   Get one post by id
+// @acces  Private if not logged in
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      // We do this bec if its equalit means that its a formated object id
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
+// @route  DELETE api/posts/:ID
+// @desc   DELETE a post by id
+// @acces  Private if not logged in
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      // We do this bec if its equalit means that its a formated object id
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Check on user
+    if (post.user.toString() !== req.user.id) {
+      //we turn post.user to a string so we can compare it to another string (req.user.id)
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    await post.remove();
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      // We do this bec if its equalit means that its a formated object id
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
