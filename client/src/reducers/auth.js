ffb49c492c9443c6eb791468d1b4e87936719d8a
Dervish Skenderi <dsk_95@hotmail.com>
2019-09-10 15:51:39 +0200

    Added logout and Navbar links

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -37,6 +38,7 @@ export default function(state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR: //does the same thing as register fail
     case LOGIN_FAIL: //remove the token set everything to null!!
+    case LOGOUT:
       localStorage.removeItem('token'); // if it failed we want to remove the token
       return {
         ...state,
