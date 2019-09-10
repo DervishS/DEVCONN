43c533e1231fd127ad72cd0a4d8eb988ee878f5f
Dervish Skenderi <dsk_95@hotmail.com>
2019-09-10 10:45:12 +0200

    Added auth reducer and Register action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,5 @@
 export const SET_ALERT = 'SET_ALERT'; // we do this so its easier to change 'SET_ALERT' and it gives a centralized file of are your action types
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
